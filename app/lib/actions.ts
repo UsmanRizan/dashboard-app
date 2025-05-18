accfaf58b237c99530611805ead0ce536401cae4
UsmanRizan <usmanrizan6@gmail.com>
2025-05-18 17:39:28 +0530

    Editing an Invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@
 import postgres from "postgres";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
@@ -14,6 +15,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoices(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -30,4 +32,23 @@ export async function createInvoices(formData: FormData) {
     `;
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
